a74f2938f30a22d3f88045e2d60038daca58f178
magabysg <gabysthrd@gmail.com>
2026-05-20 16:16:53 -0300

    first commit

--- src/Ex09.java
+++ src/Ex09.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Ex09 {
 
     void main(){
+        Scanner sc = new Scanner(System.in);
 
+        IO.println("\nDigite a letra correspondente ao conceito do aluno: ");
+        IO.println("A: Excelente" + "\n: Bom" + "\nC: Regular" + "\nD: Ruim");
+        String escolha = sc.next();
 
+        switch (escolha){
+            case "A", "a":
+                IO.println("Aluno excelente!");
+                break;
+            case "B", "b":
+                IO.println("Aluno bom!");
+                break;
+            case "C", "c":
+                IO.println("Aluno Regular.");
+                break;
+            case "D", "d":
+                IO.println("Aluno ruim.");
+                break;
+            default:
+                IO.println("Escolha uma opção valida!");
+        }
     }
 }
